80a64750990250ac5431bced1b8475365739b1ed
taileduc0404 <taileduc0404@gmail.com>
2024-03-19 13:50:03 +0700

    Update

--- PhanTichCamXucVanBan/Program.cs
+++ PhanTichCamXucVanBan/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=CamXuc}/{action=Index}/{id?}");
 
 app.Run();
